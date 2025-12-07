35d37414ba67bfaeb617912f740fc4c1c03464d4
Francesco Mignone <fmignone98@gmail.com>
2025-12-07 11:00:24 -0500

    add: full cache cbo instructions

--- examples/asm/cache/main.c
+++ examples/asm/cache/main.c
@@ -9,39 +9,83 @@ const signature_t const GOLDEN_SIGNATURES[NUMTESTS] = {
 // Info on cache instructions:
 // https://docs.riscv.org/reference/isa/v20240411/unpriv/cmo.html
 
-// cbo.flush --> flushes a cacheline 
 extern char __data_start[];
 extern char __data_end[];
 
-void flush_data_section(void) {
+// cbo.flush --> flushes a cacheline 
+
+// Flush all the cache
+void cache_flush(void) {
     char *p = __data_start;
     char *end = __data_end;
     const unsigned line = 512;   // size taken form derivlist.txt
 
     for (; p < end; p += line) {
-        __asm__ volatile ("cbo.flush (%0)" :: "r"(p) : "memory"); // actual flush
+        __asm__ volatile ("cbo.flush (%0)" :: "r"(p) : "memory");
     }
+}
+// Single cacheline flush
+void cache_flush_block(void* addr) {
+	__asm__ volatile ("cbo.flush (%0)" :: "r"(addr) : "memory");
 }
 
 //cbo.clean   --> Cleans a cache block
+
+void cache_clean(void) {
+    char *p = __data_start;
+    char *end = __data_end;
+    const unsigned line = 512;   // size taken form derivlist.txt
+
+    for (; p < end; p += line) {
+        __asm__ volatile ("cbo.clean (%0)" :: "r"(p) : "memory");
+    }
+}
+
+void cache_clean_block(void* addr) {
+	__asm__ volatile ("cbo.clean (%0)" :: "r"(addr) : "memory");
+}
+
 //cbo.inval   --> Perform an invalidate operation on a cache block
+void cache_inval(void) {
+    char *p = __data_start;
+    char *end = __data_end;
+    const unsigned line = 512;   // size taken form derivlist.txt
+
+    for (; p < end; p += line) {
+        __asm__ volatile ("cbo.inval (%0)" :: "r"(p) : "memory");
+    }
+}
+void cache_inval_block(void* addr) {
+	__asm__ volatile ("cbo.inval (%0)" :: "r"(addr) : "memory");
+}
+
 //cbo.zero    --> Store zeros to the full set of bytes corresponding to a cache block
-//prefetch.i  --> Provide a HINT to hardware that a cache block is likely to be accessed by an instruction fetch in the near future
+void cache_zero(void) {
+    char *p = __data_start;
+    char *end = __data_end;
+    const unsigned line = 512;   // size taken form derivlist.txt
+
+    for (; p < end; p += line) {
+        __asm__ volatile ("cbo.zero (%0)" :: "r"(p) : "memory");
+    }
+}
+void cache_zero_block(void* addr) {
+	__asm__ volatile ("cbo.zero (%0)" :: "r"(addr) : "memory");
+}
+
+//prefetch.i  --> Provide a HINT to hardware that a cache block is likely to be accessed by an instruction fetch in the near futures
 //prefetch.r  --> Provide a HINT to hardware that a cache block is likely to be accessed by a data read in the near future
 //prefetch.w  --> Provide a HINT to hardware that a cache block is likely to be accessed by a data write in the near future
+
+// TODO: March test function
 
 
 
 int main(void)
 {
-	int i, fails;
-	signature_t signatures[NUMTESTS];
-	
-	signatures[TEST1] = test1();
+	// test1();
 
-	for(i=0; i<NUMTESTS; i++) {
-		fails += signatures[i] != GOLDEN_SIGNATURES[i];
-	}
+    cache_zero();
 
- 	return fails;
+ 	return 0;
 }
